2f35fbd8eeae6656216ded141616aca41ff25b98
saifali3251 <alisaif3251@gmail.com>
2021-09-26 16:21:14 +0530

    Add remaiing files for Db connections

--- core/config.py
+++ core/config.py
@@ -1,9 +1,10 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from jose.constants import ALGORITHMS
+#from jose.constants import ALGORITHMS
 
 env_path = Path('.') / '.env'
+'''Above line : Python search for .env file and uses its key value pair'''
 load_dotenv(dotenv_path=env_path)
 '''This file is created for configuration of all variables in the project'''
 class Settings:
@@ -13,13 +14,13 @@ class Settings:
   POSTGRES_USER : str = os.getenv("POSTGRES_USER")
   POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
   POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER", "localhost")
-  POSTGRES_PORT : str = os.getenv("POSTGRES_PORT",5432)
+  POSTGRES_PORT : str = os.getenv("POSTGRES_PORT",5432) #default port 5432
   POSTGRES_DB : str = os.getenv("POSTGRES_DB","db_course")
   DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
-  SECRET_KEY : str =os.getenv("SECRET_KEY")
-  ALGORITHM = "HS256"
-  ACCESS_TOKEN_EXPIRE_MINUTES = 30
+  #SECRET_KEY : str =os.getenv("SECRET_KEY")
+  #ALGORITHM = "HS256"
+  #ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
 
--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from sqlalchemy.orm import session
+from core.config import settings
+from db.session import engine
+from db.base_class import Base
 
-app = FastAPI(title="Jobboard",version="0.1.0")
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+    '''Above code will connect our fastAPI with db engine'''
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_TITLE,version=settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+app = start_application()
 
 @app.get("/")
 def f1():
     return "Hello"
+
+
